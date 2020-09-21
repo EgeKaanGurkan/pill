a0afef93c580a0c6f2c99561ebd2a1f8acae071e
Ege Kaan Gürkan <ege.gurkan2608@gmail.com>
2020-09-21 12:05:55 +0300

    First commit
    The lexer and the parser should be working fine

---
+++ shell.py
@@ -0,0 +1,14 @@
+import pill
+print("Write 'exit' or 'quit' to exit pill")
+while True:
+    text = input('pill > ')
+    if text == "exit" or text == "quit":
+        print("Goodbye")
+        break
+
+    result, error = pill.run('<stdin>', text)
+
+    if error:
+        print(error.as_string())
+    else:
+        print(result)
